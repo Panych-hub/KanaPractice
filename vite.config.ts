46b7d6b7e4502cb0638e75864122e2881cd61ce7
Pavel Zamyatin <pavel.zamyatin@softcom.su>
2025-06-10 10:15:00 +0300

    fix: fix deploy problems

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/KanaPractice/', // Replace with your actual repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  }
 })
